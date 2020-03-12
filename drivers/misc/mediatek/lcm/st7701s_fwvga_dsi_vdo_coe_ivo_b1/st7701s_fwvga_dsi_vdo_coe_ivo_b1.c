e48035e987d26194968e015d4182c8ede1740af7
DevMelek <56092115+DevMelek@users.noreply.github.com>
2020-03-12 22:51:35 +0100

    Bug xd for me

--- drivers/misc/mediatek/lcm/st7701s_fwvga_dsi_vdo_coe_ivo_b1/st7701s_fwvga_dsi_vdo_coe_ivo_b1.c
+++ drivers/misc/mediatek/lcm/st7701s_fwvga_dsi_vdo_coe_ivo_b1/st7701s_fwvga_dsi_vdo_coe_ivo_b1.c
@@ -147,11 +147,11 @@ static void lcm_get_params(LCM_PARAMS *params)
 
 static void lcm_init(void)
 {
-	mt_set_gpio_out(0x80000046, GPIO_OUT_ZERO);
-	MDELAY(5);
 	mt_set_gpio_out(0x80000046, GPIO_OUT_ONE);
-	MDELAY(10);
+	MDELAY(5);
 	mt_set_gpio_out(0x80000046, GPIO_OUT_ZERO);
+	MDELAY(10);
+	mt_set_gpio_out(0x80000046, GPIO_OUT_ONE);
 	MDELAY(120);
 
 	push_table(lcm_initialization_setting, sizeof(lcm_initialization_setting) / sizeof(struct LCM_setting_table), 1);
@@ -162,16 +162,16 @@ static void lcm_suspend(void)
 {
 	push_table(lcm_deep_sleep_mode_in_setting, sizeof(lcm_deep_sleep_mode_in_setting) / sizeof(struct LCM_setting_table), 1);
 
-	mt_set_gpio_out(0x80000046, GPIO_OUT_ONE);
+	mt_set_gpio_out(0x80000046, GPIO_OUT_ZERO);
 }
 
 static void lcm_resume(void)
 {
-	mt_set_gpio_out(0x80000046, GPIO_OUT_ZERO);
-	MDELAY(5);
 	mt_set_gpio_out(0x80000046, GPIO_OUT_ONE);
+	MDELAY(5);
+	mt_set_gpio_out(0x80000046, GPIO_OUT_ZERO);
 	MDELAY(10);
-	mt_set_gpio_out(0x80000046, GPIO_OUT_ZERO;
+	mt_set_gpio_out(0x80000046, GPIO_OUT_ONE);
 	MDELAY(10);
 
 	push_table(lcm_initialization_setting, sizeof(lcm_initialization_setting) / sizeof(struct LCM_setting_table), 1);
